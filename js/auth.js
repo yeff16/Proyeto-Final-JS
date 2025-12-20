6c7559af1e1ccbd3265ac99ffca160198c53d973
yeff16 <yefersone.pineda1@gmail.com>
2025-12-20 00:45:26 -0400

    add changes

--- js/auth.js
+++
@@ -1,4 +0,0 @@
-// Verificacion de si el usuario es administrador mediante una clave llamada "Chocolate"
-// Compara la clave ingresada por el usuario con la clave "Chocolate" para determinar si el usuario es administrador si la clave es correcta se le permite el acceso
-// Si el usuario es administrador, se redirecciona a la pagina de administracion de usuarios
-// Si no es administrador, se redirige al apartado de usuario comun.
--- js/validations.js
+++
@@ -1,6 +0,0 @@
-// Validaciones de formularios (login, register, CRUD).
-//Validar campos vacíos.
-//alidar formato de email.
-//Evitar registro de emails repetidos (localStorage.getItem("users")).
-//Validar contraseñas seguras (mínimo longitud).
-//Mostrar mensajes de error amigables.
